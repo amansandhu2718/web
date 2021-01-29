6702cfba2396a3287c3a09a3eadb6b0946921af5
amansandhu2718 <77589604+amansandhu2718@users.noreply.github.com>
2021-01-29 10:43:46 +0530

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,34 +1,20 @@
 
-
-   filterSelection("all");
-   function filterSelection(c) {
-     var x, i;
-     x = document.getElementsByClassName("filterDiv");
-     if (c == "all") c = "";
-     for (i = 0; i < x.length; i++) {
-       RemoveClass(x[i], "show");
-       if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+   var mybutton = document.getElementById("myBtn");
+   
+   
+   window.onscroll = function() {scrollFunction()};
+   
+   function scrollFunction() {
+     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+       mybutton.style.display = "block";
+     } else {
+       mybutton.style.display = "none";
      }
    }
    
-   function AddClass(element, name) {
-     var i, arr1, arr2;
-     arr1 = element.className.split(" ");
-     arr2 = name.split(" ");
-     for (i = 0; i < arr2.length; i++) {
-       if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-     }
+   // When the user clicks on the button, scroll to the top of the document
+   function topFunction() {
+     document.body.scrollTop = 0;
+     document.documentElement.scrollTop = 0;
    }
-   
-   function RemoveClass(element, name) {
-     var i, arr1, arr2;
-     arr1 = element.className.split(" ");
-     arr2 = name.split(" ");
-     for (i = 0; i < arr2.length; i++) {
-       while (arr1.indexOf(arr2[i]) > -1) {
-         arr1.splice(arr1.indexOf(arr2[i]), 1);     
-       }
-     }
-     element.className = arr1.join(" ");
-   }
-   
+  
