52af03f4e1b1935d7a97bf197a6d9b4a92ff1b14
amansandhu2718 <77589604+amansandhu2718@users.noreply.github.com>
2021-02-03 14:17:29 +0530

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -2,15 +2,9 @@
    var mybutton = document.getElementsByClassName("myBtn");
    
    
-   window.onscroll = function() {scrollFunction()};
+
    
-   function scrollFunction() {
-     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-       mybutton.style.display = "block";
-     } else {
-       mybutton.style.display = "none";
-     }
-   }
+   
    
    function topFunction() {
      document.body.scrollTop = 0;
